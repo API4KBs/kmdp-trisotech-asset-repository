fd1d6243c4e54ed72d38c7404cc807a9a9964455
Cheryl Orcutt (M188263) <orcutt.cheryl@mayo.edu>
2019-07-29 10:11:09 -0500

    Updated to release
    Updated pom dependencies to the released version of the support
    libraries. One file was updated to spaces vs tabs.

--- kmdp-trisotech-tools/trisotech-metadata-extractor/src/test/java/edu/mayo/kmdp/SemanticAnnotationTest.java
+++ kmdp-trisotech-tools/trisotech-metadata-extractor/src/test/java/edu/mayo/kmdp/SemanticAnnotationTest.java
@@ -41,23 +41,23 @@ import static org.junit.jupiter.api.Assertions.*;
 class SemanticAnnotationTest {
 
 
-	private static MetadataExtractor extractor = new MetadataExtractor();
-	private static Weaver dmnWeaver;
+  private static MetadataExtractor extractor = new MetadataExtractor();
+  private static Weaver dmnWeaver;
 
-	@BeforeAll
-	public static void init() {
-		dmnWeaver = new Weaver( );
+  @BeforeAll
+  public static void init() {
+    dmnWeaver = new Weaver( );
 //		SemanticAnnotationTest.class.getResourceAsStream("/TestDictionary.xlsx"),
 //		                                       false,
 //		                                       DictionaryEntryWeaver.getWeaverProperties( KnowledgeRepresentationLanguage.DMN_1_1 ) );
-	}
+  }
 
 
 
 
-	@Disabled("testExtraction: Current_Chronological_Age no longer exists...need input files updated to Trisotech")
-	@Test
-	void testExtraction() {
+  @Disabled("testExtraction: Current_Chronological_Age no longer exists...need input files updated to Trisotech")
+  @Test
+  void testExtraction() {
 //		String dmnPath = "/ComputableDM.dmn";
 //		String metaPath = "/ComputableDM_Info.json";
 //
@@ -83,132 +83,132 @@ class SemanticAnnotationTest {
 //			e.printStackTrace();
 //			fail( e.getMessage() );
 //		}
-	}
+  }
 
 
-	@Disabled("testSalienceNonExtraction needs input files updated to Trisotech")
-	@Test
-	void testSalienceNonExtraction() {
-		String dmnPath = "/Salient.dmn";
-		String metaPath = "/Salient_Info.json";
+  @Disabled("testSalienceNonExtraction needs input files updated to Trisotech")
+  @Test
+  void testSalienceNonExtraction() {
+    String dmnPath = "/Salient.dmn";
+    String metaPath = "/Salient_Info.json";
 
-		Optional<byte[]> dmn = XMLUtil.loadXMLDocument( SemanticAnnotationTest.class.getResourceAsStream(dmnPath))
-		                              .map( dmnWeaver::weave )
-		                              .map( XMLUtil::toByteArray );
-		assertTrue( dmn.isPresent() );
+    Optional<byte[]> dmn = XMLUtil.loadXMLDocument( SemanticAnnotationTest.class.getResourceAsStream(dmnPath))
+        .map( dmnWeaver::weave )
+        .map( XMLUtil::toByteArray );
+    assertTrue( dmn.isPresent() );
 
 
-		try {
-			Optional<KnowledgeAsset> res = extractor.extract(new ByteArrayInputStream(dmn.get() ),
-			                                                 SemanticAnnotationTest.class.getResourceAsStream(metaPath));
-			if ( ! res.isPresent() ) {
-				fail( "Unable to instantiate metadata object" );
-			}
-			KnowledgeAsset surr = res.get();
-			List<Annotation> annos = surr.getSubject();
-			assertEquals(0, annos.size() );
-		} catch ( Exception e ) {
-			e.printStackTrace();
-			fail( e.getMessage() );
-		}
-	}
+    try {
+      Optional<KnowledgeAsset> res = extractor.extract(new ByteArrayInputStream(dmn.get() ),
+          SemanticAnnotationTest.class.getResourceAsStream(metaPath));
+      if ( ! res.isPresent() ) {
+        fail( "Unable to instantiate metadata object" );
+      }
+      KnowledgeAsset surr = res.get();
+      List<Annotation> annos = surr.getSubject();
+      assertEquals(0, annos.size() );
+    } catch ( Exception e ) {
+      e.printStackTrace();
+      fail( e.getMessage() );
+    }
+  }
 
 
   @Disabled("testExtractionFull: FIX")
-	@Test
-	void testExtractionFull() {
-		String dmnPath = "/WeaverTest1.dmn";
-		String metaPath = "/WeaverTest1Meta.json";
+  @Test
+  void testExtractionFull() {
+    String dmnPath = "/WeaverTest1.dmn";
+    String metaPath = "/WeaverTest1Meta.json";
 
-		Optional<byte[]> dmn = XMLUtil.loadXMLDocument( SemanticAnnotationTest.class.getResourceAsStream(dmnPath))
-		                              .map( dmnWeaver::weave )
-		                              .map( XMLUtil::toByteArray );
-		assertTrue( dmn.isPresent() );
-		try {
-			Optional<KnowledgeAsset> res = extractor.extract(new ByteArrayInputStream(dmn.get() ),
-			                                                 SemanticAnnotationTest.class.getResourceAsStream(metaPath));
-			if ( ! res.isPresent() ) {
-				fail( "Unable to instantiate metadata object" );
-			}
-			KnowledgeAsset surr = res.get();
-			// long form
-			List<Annotation> annotations = surr.getSubject();
-			for (Annotation anno: annotations
-					 ) {
-				System.out.println("anno is : " + anno.getClass() + " annotation");
-				System.out.println("anno rel: " + anno.getRel().toString());
-				System.out.println("AnnotationRelType.In_Terms_Of.asConcept(): " + AnnotationRelType.In_Terms_Of.asConcept());
-				System.out.println("((SimpleAnnotation)anno).getExpr()" + ((SimpleAnnotation)anno).getExpr());
-				System.out.println("ClinicalSituation.resolve(anno.getExpr()): " + ClinicalSituation.resolve(((SimpleAnnotation)anno).getExpr()));
-				System.out.println("ClinicalSituation name: " + ClinicalSituation.resolve(((SimpleAnnotation)anno).getExpr()).get().name());
+    Optional<byte[]> dmn = XMLUtil.loadXMLDocument( SemanticAnnotationTest.class.getResourceAsStream(dmnPath))
+        .map( dmnWeaver::weave )
+        .map( XMLUtil::toByteArray );
+    assertTrue( dmn.isPresent() );
+    try {
+      Optional<KnowledgeAsset> res = extractor.extract(new ByteArrayInputStream(dmn.get() ),
+          SemanticAnnotationTest.class.getResourceAsStream(metaPath));
+      if ( ! res.isPresent() ) {
+        fail( "Unable to instantiate metadata object" );
+      }
+      KnowledgeAsset surr = res.get();
+      // long form
+      List<Annotation> annotations = surr.getSubject();
+      for (Annotation anno: annotations
+      ) {
+        System.out.println("anno is : " + anno.getClass() + " annotation");
+        System.out.println("anno rel: " + anno.getRel().toString());
+        System.out.println("AnnotationRelType.In_Terms_Of.asConcept(): " + AnnotationRelType.In_Terms_Of.asConcept());
+        System.out.println("((SimpleAnnotation)anno).getExpr()" + ((SimpleAnnotation)anno).getExpr());
+        System.out.println("ClinicalSituation.resolve(anno.getExpr()): " + ClinicalSituation.resolve(((SimpleAnnotation)anno).getExpr()));
+        System.out.println("ClinicalSituation name: " + ClinicalSituation.resolve(((SimpleAnnotation)anno).getExpr()).get().name());
 
-				if(anno.getClass().isInstance(SimpleAnnotation.class)) {
-					System.out.println("anno is SimpleAnnotation: " + anno.toString());
-					System.out.println("anno rel: " + anno.getRel().toString());
-				}
-			}
-			List<ClinicalSituation> inputs = surr.getSubject().stream()
-			                       .filter(SimpleAnnotation.class::isInstance)
-			                       .map( SimpleAnnotation.class::cast)
-			                       .filter( (a) -> a.getRel().equals( AnnotationRelType.In_Terms_Of.asConcept() ) )
-			                       .map( (a) -> ClinicalSituation.resolve( a.getExpr() ) )
-			                       .map( Optional::get )
-			                       .collect(Collectors.toList());
+        if(anno.getClass().isInstance(SimpleAnnotation.class)) {
+          System.out.println("anno is SimpleAnnotation: " + anno.toString());
+          System.out.println("anno rel: " + anno.getRel().toString());
+        }
+      }
+      List<ClinicalSituation> inputs = surr.getSubject().stream()
+          .filter(SimpleAnnotation.class::isInstance)
+          .map( SimpleAnnotation.class::cast)
+          .filter( (a) -> a.getRel().equals( AnnotationRelType.In_Terms_Of.asConcept() ) )
+          .map( (a) -> ClinicalSituation.resolve( a.getExpr() ) )
+          .map( Optional::get )
+          .collect(Collectors.toList());
 
-			// TODO: update per model CAO
-			assertEquals( 19, inputs.size() );
-			assertTrue( inputs.contains( ClinicalSituation.History_Of_GI_Bleeding ) ); // was .Has_Bleeding_Disorder CAO
+      // TODO: update per model CAO
+      assertEquals( 19, inputs.size() );
+      assertTrue( inputs.contains( ClinicalSituation.History_Of_GI_Bleeding ) ); // was .Has_Bleeding_Disorder CAO
 //			assertTrue( inputs.contains( ClinicalSituation.Has_Cirrhosis ) );
-		} catch ( Exception e ) {
-			e.printStackTrace();
-			fail( e.getMessage() );
-		}
-	}
+    } catch ( Exception e ) {
+      e.printStackTrace();
+      fail( e.getMessage() );
+    }
+  }
 
 
-	@Disabled("testExtractionFull2 can't be tested until input files are created/updated to Trisotech")
-	@Test
-	void testExtractionFull2() {
-		String dmnPath = "/TBD.dmn";
-		String metaPath = "/TBD_Info.json";
+  @Disabled("testExtractionFull2 can't be tested until input files are created/updated to Trisotech")
+  @Test
+  void testExtractionFull2() {
+    String dmnPath = "/TBD.dmn";
+    String metaPath = "/TBD_Info.json";
 
-		Optional<byte[]> dmn = XMLUtil.loadXMLDocument( SemanticAnnotationTest.class.getResourceAsStream(dmnPath))
-		                              .map( dmnWeaver::weave )
-		                              .map( XMLUtil::toByteArray );
-		assertTrue( dmn.isPresent() );
-		try {
-			Optional<KnowledgeAsset> res = extractor.extract(new ByteArrayInputStream(dmn.get() ),
-			                                                 SemanticAnnotationTest.class.getResourceAsStream(metaPath));
-			if ( ! res.isPresent() ) {
-				fail( "Unable to instantiate metadata object" );
-			}
-			KnowledgeAsset surr = res.get();
-			List<ClinicalSituation> inputs = surr.getSubject().stream()
-			                       .filter(SimpleAnnotation.class::isInstance)
-			                       .map( SimpleAnnotation.class::cast)
-			                       .filter( (a) -> a.getRel().equals( AnnotationRelType.In_Terms_Of.asConcept() ) )
-			                       .map( (a) -> ClinicalSituation.resolve( a.getExpr() ) )
-			                       .map( Optional::get )
-			                       .collect(Collectors.toList());
+    Optional<byte[]> dmn = XMLUtil.loadXMLDocument( SemanticAnnotationTest.class.getResourceAsStream(dmnPath))
+        .map( dmnWeaver::weave )
+        .map( XMLUtil::toByteArray );
+    assertTrue( dmn.isPresent() );
+    try {
+      Optional<KnowledgeAsset> res = extractor.extract(new ByteArrayInputStream(dmn.get() ),
+          SemanticAnnotationTest.class.getResourceAsStream(metaPath));
+      if ( ! res.isPresent() ) {
+        fail( "Unable to instantiate metadata object" );
+      }
+      KnowledgeAsset surr = res.get();
+      List<ClinicalSituation> inputs = surr.getSubject().stream()
+          .filter(SimpleAnnotation.class::isInstance)
+          .map( SimpleAnnotation.class::cast)
+          .filter( (a) -> a.getRel().equals( AnnotationRelType.In_Terms_Of.asConcept() ) )
+          .map( (a) -> ClinicalSituation.resolve( a.getExpr() ) )
+          .map( Optional::get )
+          .collect(Collectors.toList());
 
-			assertEquals( 9, inputs.size() );
-			assertTrue( inputs.contains( ClinicalSituation.Sex ) );
+      assertEquals( 9, inputs.size() );
+      assertTrue( inputs.contains( ClinicalSituation.Sex ) );
 //			assertTrue( inputs.contains( ClinicalSituation.Recent_History_Of_TIA ) );
 //			assertTrue( inputs.contains( ClinicalSituation.History_Of_Vascular_Disease ) );
 
-			Set<ConceptIdentifier> defines  = surr.getSubject().stream()
-			                                      .filter( (ann) -> ann.getRel().equals( AnnotationRelType.Defines.asConcept() ) )
-			                                      .map( (ann) -> ((SimpleAnnotation) ann).getExpr() )
-			                                      .collect(Collectors.toSet());
-			assertEquals( 3, defines.size() );
-			assertTrue( defines.contains( ClinicalSituation.Current_CHA2DS2_VASc_Score.asConcept() ) );
-			assertTrue( defines.contains( ClinicalSituation.Risk_Of_Embolic_Stroke_CHA2DS2_VASc1.asConcept() ) ); // was: Likelihood_Of_Stroke_Based_On_CHA2DS2Vasc_Score CAO
+      Set<ConceptIdentifier> defines  = surr.getSubject().stream()
+          .filter( (ann) -> ann.getRel().equals( AnnotationRelType.Defines.asConcept() ) )
+          .map( (ann) -> ((SimpleAnnotation) ann).getExpr() )
+          .collect(Collectors.toSet());
+      assertEquals( 3, defines.size() );
+      assertTrue( defines.contains( ClinicalSituation.Current_CHA2DS2_VASc_Score.asConcept() ) );
+      assertTrue( defines.contains( ClinicalSituation.Risk_Of_Embolic_Stroke_CHA2DS2_VASc1.asConcept() ) ); // was: Likelihood_Of_Stroke_Based_On_CHA2DS2Vasc_Score CAO
 
-		} catch ( Exception e ) {
-			e.printStackTrace();
-			fail( e.getMessage() );
-		}
-	}
+    } catch ( Exception e ) {
+      e.printStackTrace();
+      fail( e.getMessage() );
+    }
+  }
 
 
 

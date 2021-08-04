bc1abb866f747a7a586f3b13d0cd977c895a717e
Davide Sottara <Sottara.Davide@mayo.edu>
2021-08-04 02:58:19 -0500

    [#659305] Fix test to work on AzDO pipeline

--- kmdp-trisotech-asset-repository-service-impl/src/test/java/edu/mayo/kmdp/trisotech/TrisotechAssetRepositoryClientTest.java
+++ kmdp-trisotech-asset-repository-service-impl/src/test/java/edu/mayo/kmdp/trisotech/TrisotechAssetRepositoryClientTest.java
@@ -36,6 +36,7 @@ import org.springframework.test.context.TestPropertySource;
 @ContextConfiguration(classes = IntegrationTestConfig.class)
 @TestPropertySource(properties = {
     "edu.mayo.kmdp.trisotechwrapper.repositoryName=MEA-Test",
+    "edu.mayo.kmdp.trisotechwrapper.repositoryPath=/",
     "edu.mayo.kmdp.trisotechwrapper.repositoryId=d4aca01b-d446-4bc8-a6f0-85d84f4c1aaf",
     "edu.mayo.kmdp.trisotechwrapper.baseUrl=https://mc.trisotech.com/"})
 class TrisotechAssetRepositoryClientTest {

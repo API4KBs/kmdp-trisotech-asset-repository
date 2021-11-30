8ad8e5d0856a0e6a98ea9602e73ede41b636bfa2
Davide Sottara <Sottara.Davide@mayo.edu>
2021-11-30 12:03:30 -0600

    [#1153119] Move Health assessment logic for Terms provider into the source project

--- kmdp-trisotech-asset-repository-service-impl/src/main/java/edu/mayo/kmdp/kdcaci/knew/trisotech/TTMonitoringConfig.java
+++ kmdp-trisotech-asset-repository-service-impl/src/main/java/edu/mayo/kmdp/kdcaci/knew/trisotech/TTMonitoringConfig.java
@@ -4,15 +4,11 @@ import edu.mayo.kmdp.health.datatype.ApplicationComponent;
 import edu.mayo.kmdp.health.datatype.MiscProperties;
 import edu.mayo.kmdp.health.datatype.Status;
 import edu.mayo.kmdp.health.utils.MonitorUtil;
+import edu.mayo.kmdp.terms.TermsHealthUtils;
 import edu.mayo.kmdp.trisotechwrapper.TrisotechWrapper;
-import java.util.List;
 import java.util.Map;
 import java.util.function.Supplier;
-import java.util.stream.Collectors;
-import org.omg.spec.api4kp._20200801.Answer;
 import org.omg.spec.api4kp._20200801.api.terminology.v4.server.TermsApiInternal;
-import org.omg.spec.api4kp._20200801.id.Pointer;
-import org.omg.spec.api4kp._20200801.id.ResourceIdentifier;
 import org.omg.spec.api4kp._20200801.services.KPComponent;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -32,41 +28,10 @@ public class TTMonitoringConfig {
   @Value("${edu.mayo.kmdp.trisotechwrapper.repositoryId:nil}")
   String mainPlaceId;
 
-  @Value("${edu.mayo.kmdp.kasrs.repository.defaultRepoUrl:n/a}")
-  String mainKarsUrl;
-
   @Bean
   Supplier<ApplicationComponent> terms(@Autowired @KPComponent(implementation = "broker")
       TermsApiInternal terms) {
-    MiscProperties mp = new MiscProperties();
-    mp.put("edu.mayo.kmdp.kasrs.repository.defaultRepoUrl", mainKarsUrl);
-    return () -> {
-      ApplicationComponent c = new ApplicationComponent();
-      c.setName("Terminology Provideer");
-      if (terms == null) {
-        c.setStatusMessage("No Terminology Provider Available");
-        c.setStatus(Status.DOWN);
-      } else if (mainKarsUrl == null) {
-        c.setStatusMessage("No KARS source - falling back to classpath resources");
-        c.setStatus(Status.IMPAIRED);
-      } else {
-        Answer<List<Pointer>> terminologies = terms.listTerminologies();
-        if (terminologies.isFailure()) {
-          c.setStatusMessage("Unable to access vocabularies : " + terminologies.printExplanation());
-          c.setStatus(Status.IMPAIRED);
-        } else if (terminologies.get().isEmpty()) {
-          c.setStatusMessage("No Terminologies available");
-          c.setStatus(Status.IMPAIRED);
-        } else {
-          c.setStatusMessage("Active vocabularies : " + terminologies.get().stream()
-              .map(ResourceIdentifier::getTag).collect(Collectors.joining(","))
-          );
-          c.setStatus(Status.UP);
-        }
-      }
-      c.setDetails(mp);
-      return c;
-    };
+    return TermsHealthUtils.diagnoseTermsServer(terms);
   }
 
   @Bean

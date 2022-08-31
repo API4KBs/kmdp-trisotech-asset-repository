d17b83c574e1174dfcaee63b79089c8732a93baf
Davide Sottara <dsotty@gmail.com>
2022-08-31 12:59:52 -0500

    [#1780295] Health endpoint should use the correct Terms provider, and obfuscate secrets

--- kmdp-trisotech-asset-repository-service-impl/src/main/java/edu/mayo/kmdp/kdcaci/knew/trisotech/TTMonitoringConfig.java
+++ kmdp-trisotech-asset-repository-service-impl/src/main/java/edu/mayo/kmdp/kdcaci/knew/trisotech/TTMonitoringConfig.java
@@ -1,51 +1,61 @@
 package edu.mayo.kmdp.kdcaci.knew.trisotech;
 
+import edu.mayo.kmdp.health.HealthEndPoint;
+import edu.mayo.kmdp.health.StateEndPoint;
+import edu.mayo.kmdp.health.VersionEndPoint;
 import edu.mayo.kmdp.health.datatype.ApplicationComponent;
 import edu.mayo.kmdp.health.datatype.MiscProperties;
 import edu.mayo.kmdp.health.datatype.Status;
 import edu.mayo.kmdp.health.utils.MonitorUtil;
+import edu.mayo.kmdp.terms.TermsFHIRFacade;
 import edu.mayo.kmdp.terms.health.TermsHealthService;
 import edu.mayo.kmdp.trisotechwrapper.TrisotechWrapper;
 import java.util.Map;
 import java.util.function.Supplier;
 import org.omg.spec.api4kp._20200801.api.terminology.v4.server.TermsApiInternal;
 import org.omg.spec.api4kp._20200801.services.KPComponent;
-import org.omg.spec.api4kp._20200801.services.KPServer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.ConfigurableEnvironment;
 
 
+@ComponentScan(basePackageClasses = {
+    TrisotechWrapper.class,
+    TermsFHIRFacade.class,
+    HealthEndPoint.class,
+    StateEndPoint.class,
+    VersionEndPoint.class})
 @Configuration
 @PropertySource(value = {"classpath:application.properties"})
 public class TTMonitoringConfig {
-
-  @Autowired
-  ConfigurableEnvironment env;
 
   @Value("${edu.mayo.kmdp.trisotechwrapper.repositoryId:nil}")
   String mainPlaceId;
 
   @Bean
   public Supplier<ApplicationComponent> termsHealth(
-      @Autowired @KPServer @KPComponent(implementation = "broker") TermsApiInternal termsApiInternal) {
-
+      @Autowired @KPComponent(implementation = "fhir") TermsApiInternal termsApiInternal) {
     return () -> {
-
       TermsHealthService termsHealthService = new TermsHealthService(termsApiInternal);
       return termsHealthService.assessHealth();
-
     };
-
   }
 
   @Bean
-  Supplier<ApplicationComponent> ttServer(@Autowired TrisotechWrapper client) {
+  Supplier<ApplicationComponent> ttServer(
+      @Autowired ConfigurableEnvironment env,
+      @Autowired TrisotechWrapper client) {
     MiscProperties mp = new MiscProperties();
-    mp.putAll(MonitorUtil.getAppProperties(env));
+    MonitorUtil.getAppProperties(env).forEach((prop, value) -> {
+      var safeVal = MonitorUtil.defaultIsSecret(prop)
+          ? MonitorUtil.obfuscate(value, 4)
+          : value;
+      mp.put(prop, safeVal);
+    });
 
     return () -> {
       ApplicationComponent c = new ApplicationComponent();

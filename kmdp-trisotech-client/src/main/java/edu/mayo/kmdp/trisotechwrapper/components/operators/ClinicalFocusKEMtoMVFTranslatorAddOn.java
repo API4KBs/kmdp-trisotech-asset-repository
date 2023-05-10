3410a959c4fc998755cc6e7ca03561029dfc7933
Davide Sottara <dsotty@gmail.com>
2023-05-10 07:35:29 -0500

    [#2217163] Temp Clone SCTTags enum to avoid classloading issues

--- kmdp-trisotech-client/src/main/java/edu/mayo/kmdp/trisotechwrapper/components/operators/ClinicalFocusKEMtoMVFTranslatorAddOn.java
+++ kmdp-trisotech-client/src/main/java/edu/mayo/kmdp/trisotechwrapper/components/operators/ClinicalFocusKEMtoMVFTranslatorAddOn.java
@@ -9,6 +9,7 @@ import edu.mayo.kmdp.util.NameUtils;
 import java.util.Arrays;
 import java.util.Map;
 import java.util.Optional;
+import java.util.Set;
 import java.util.UUID;
 import java.util.stream.Collectors;
 import javax.annotation.Nonnull;
@@ -17,7 +18,6 @@ import org.omg.spec.mvf._20220702.mvf.MVFElement;
 import org.omg.spec.mvf._20220702.mvf.MVFEntry;
 import org.omg.spec.mvf._20220702.mvf.Vocabulary;
 import org.omg.spec.mvf._20220702.mvf.VocabularyEntry;
-import org.snomed.SCTCategories;
 
 /**
  * {@link KEMtoMVFTranslatorExtension} that interprets KEM Concepts annotated with SNOMED Codes,
@@ -37,6 +37,13 @@ public class ClinicalFocusKEMtoMVFTranslatorAddOn
   private static final String SNOMED_NS = "http://snomed.info/id/";
   private static final String SNOMED_NAME = "SNOMEDCT";
 
+  private static final Set<String> sctTags;
+
+  static {
+    sctTags = Arrays.stream(SCTCategories.values())
+        .map(SCTCategories::getValue)
+        .collect(Collectors.toSet());
+  }
 
   /**
    * @param dict        the generated MVF model
@@ -87,10 +94,7 @@ public class ClinicalFocusKEMtoMVFTranslatorAddOn
         && getTags(kc).stream()
         .filter(tag -> tag.startsWith("sct:"))
         .map(tag -> tag.substring(4))
-        .anyMatch(t ->
-            Arrays.stream(SCTCategories.values())
-                .map(SCTCategories::getValue)
-                .anyMatch(ct -> ct.equals(t)));
+        .anyMatch(sctTags::contains);
   }
 
   /**
@@ -238,5 +242,76 @@ public class ClinicalFocusKEMtoMVFTranslatorAddOn
         .withExternalReference(x.getUri()));
   }
 
+
+  // FIXME
+  // This is a clone of SCTCategories in kmdp-standards-models/ihtdso-scg
+  // When used from the external module, may raise a NoClassDefFound exception
+  public enum SCTCategories {
+    BODY_STRUCTURE("body structure"),
+    CELL("cell"),
+    CELL_STRUCTURE("cell structure"),
+    MORPHOLOGIC_ABNORMALITY("morphologic abnormality"),
+    FINDING("finding"),
+    DISORDER("disorder"),
+    ENVIRONMENT_LOCATION("environment / location"),
+    ENVIRONMENT("environment"),
+    GEOGRAPHIC_LOCATION("geographic location"),
+    EVENT("event"),
+    OBSERVABLE_ENTITY("observable entity"),
+    ORGANISM("organism"),
+    CLINICAL_DRUG("clinical drug"),
+    MEDICINAL_PRODUCT("medicinal product"),
+    MEDICINAL_PRODUCT_FORM("medicinal product form"),
+    PHYSICAL_OBJECT("physical object"),
+    PRODUCT("product"),
+    PHYSICAL_FORCE("physical force"),
+    PROCEDURE("procedure"),
+    REGIME_THERAPY("regime/therapy"),
+    QUALIFIER_VALUE("qualifier value"),
+    ADMINISTRATION_METHOD("administration method"),
+    BASIC_DOSE_FORM("basic dose form"),
+    DISPOSITION("disposition"),
+    DOSE_FORM("dose form"),
+    INTENDED_SITE("intended site"),
+    NUMBER("number"),
+    PRODUCT_NAME("product name"),
+    RELEASE_CHARACTERISTIC("release characteristic"),
+    ROLE("role"),
+    STATE_OF_MATTER("state of matter"),
+    TRANSFORMATION("transformation"),
+    SUPPLIER("supplier"),
+    UNIT_OF_PRESENTATION("unit of presentation"),
+    RECORD_ARTIFACT("record artifact"),
+    SITUATION("situation"),
+    ATTRIBUTE("attribute"),
+    CORE_METADATA_CONCEPT("core metadata concept"),
+    FOUNDATION_METADATA_CONCEPT("foundation metadata concept"),
+    LINK_ASSERTION("link assertion"),
+    LINKAGE_CONCEPT("linkage concept"),
+    NAMESPACE_CONCEPT("namespace concept"),
+    SOCIAL_CONCEPT("social concept"),
+    ETHNIC_GROUP("ethnic group"),
+    LIFE_STYLE("life style"),
+    OCCUPATION("occupation"),
+    PERSON("person"),
+    RACIAL_GROUP("racial group"),
+    RELIGION_PHILOSOPHY("religion/philosophy"),
+    NAVIGATIONAL_CONCEPT("navigational concept"),
+    SPECIMEN("specimen"),
+    STAGING_SCALE("staging scale"),
+    ASSESSMENT_SCALE("assessment scale"),
+    TUMOR_STAGING("tumor staging"),
+    SUBSTANCE("substance");
+
+    private final String value;
+
+    SCTCategories(String value) {
+      this.value = value;
+    }
+
+    public String getValue() {
+      return value;
+    }
+  }
 
 }
